2013ad29baafff1c3ea77b14784c1ff7da1ff779
birdgang82 <birdgang82@gmail.com>
2022-09-12 21:50:17 +0900

    init code.

---
+++ scripts/deploy-erc20-token.js
@@ -0,0 +1,19 @@
+const {ethers} = require("hardhat");
+
+async function main() {
+    let [deployer] = await ethers.getSigners();
+    const network = await ethers.provider.getNetwork()
+    console.log(`chainId : ${network.chainId}) `)
+    console.log('Deploying contracts with the account :', deployer.address);
+
+    const Token = await ethers.getContractFactory("BirdgangToken");
+    const token = await Token.deploy();
+    console.log('Token address :', token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
